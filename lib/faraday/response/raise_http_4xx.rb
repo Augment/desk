0a747f4e27e33108dce6503c14b8aec8682d6375
cedric darricau <cedric.darricau@augmentedev.com>
2017-11-30 16:30:07 +0100

    Add check for error response body['raw']['message']

--- lib/faraday/response/raise_http_4xx.rb
+++ lib/faraday/response/raise_http_4xx.rb
@@ -38,12 +38,40 @@ module Faraday
         ": #{body['error']}"
       elsif body['errors']
         first = body['errors'].to_a.first
-        if first.kind_of? Hash
+        if first.is_a? Hash
           ": #{first['message'].chomp}"
         else
           ": #{first.chomp}"
         end
+      elsif body['raw']
+        error_message = ": #{body['raw']['message']}"
+        return error_message unless body.errors.present?
+
+        field, code = body.errors.first
+        while code.nil? || (words = validation_error_code_in_words(field, code.first)).nil? do
+          if code.first.is_a? String
+            f, code = code.first
+          else
+            f, code = code.first.to_a.first
+          end
+        end
+        error_message += words.to_s
+        error_message
       end
+    end
+
+    def validation_error_code_in_words(field, code)
+      return unless code.is_a? String
+      {
+        blank: ": #{field}: value has not been set and is required",
+        existence: ": #{field}: value does not exist",
+        taken: ": #{field}: value has already been taken and must be unique",
+        inclusion: ": #{field}: value is not an available choice",
+        exclusion: ": #{field}: value is reserved",
+        too_short: ": #{field}: string value is too short",
+        too_long: ": #{field}: string value is too long",
+        invalid: ": #{field}: value is invalid, please see documentation for resource specifics"
+      }[code.to_sym]
     end
   end
 end
